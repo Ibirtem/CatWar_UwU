4b9b28decd24f8366d104334c0740edaec18c15c
Ibi_Tea <hyvak52@gmail.com>
2025-10-07 19:04:55 +0300

    feat: Remove old stuff

--- modules/music.js
+++
@@ -1,9 +0,0 @@
-setupSingleCallback("#cages_div", playMusic);
-
-function playMusic() {
-        soundManager.loadSound(
-            "mountsound",
-            "https://github.com/Ibirtem/CatWar/raw/main/sounds/mountain-wind-rushing-wind.mp3",
-        );
-        soundManager.playSound("mountsound", "5", true);
-}
